35ffe2fad188317b26c75661c62f1045446dddb8
dbdu <1797890817@qq.com>
2018-07-08 11:10:29 +0800

    修改适配，让代码不乱码并可以运行！

--- JavaConcurrentProgrammingCode/src/geym/conc/ch1/MultiThreadLong.java
+++ JavaConcurrentProgrammingCode/src/geym/conc/ch1/MultiThreadLong.java
@@ -1,41 +1,46 @@
 package geym.conc.ch1;
 
 /**
- * @author Administrator
+ * Description:32位的虚拟机，可能会打印内容出来，以为32位虚拟机中long的操作不是原子性的，可能会出错！
  *
+ * @author Administrator
  */
 public class MultiThreadLong {
-	public volatile static long t=0;
-	public static class ChangeT implements Runnable{
-		private long to;
-		public ChangeT(long to){
-			this.to=to;
-		}
-		@Override
-		public void run() {
-			while(true){
-			MultiThreadLong.t=to;
-			Thread.yield();
-			}
-		}
-	}
-	public static class ReadT implements Runnable{
-		@Override
-		public void run() {
-			while(true){
-			 long tmp=MultiThreadLong.t;
-			 if(tmp!=111L && tmp!=-999L && tmp!=333L && tmp!=-444L)
-			     System.out.println(tmp);
-			Thread.yield();
-			}
-		}
-	}
-	
-	public static void main(String[] args) {
-		new Thread(new ChangeT(111L)).start();
-		new Thread(new ChangeT(-999L)).start();
-		new Thread(new ChangeT(333L)).start();
-		new Thread(new ChangeT(-444L)).start();
-		new Thread(new ReadT()).start();
-	}
+    public volatile static long t = 0;
+
+    public static class ChangeT implements Runnable {
+        private long to;
+
+        public ChangeT(long to) {
+            this.to = to;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                MultiThreadLong.t = to;
+                Thread.yield();
+            }
+        }
+    }
+
+    public static class ReadT implements Runnable {
+        @Override
+        public void run() {
+            while (true) {
+                long tmp = MultiThreadLong.t;
+                if (tmp != 111L && tmp != -999L && tmp != 333L && tmp != -444L)
+                    System.out.println(tmp);
+                Thread.yield();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new Thread(new ChangeT(111L)).start();
+        new Thread(new ChangeT(-999L)).start();
+        new Thread(new ChangeT(333L)).start();
+        new Thread(new ChangeT(-444L)).start();
+        new Thread(new ReadT()).start();
+    }
 }
